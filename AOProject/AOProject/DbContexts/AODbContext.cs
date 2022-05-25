a6be6eaea7de45822d8e3ace94c21ebe72000be1
WAMBUI MUCHERU <wmucheru@microsoft.com>
2022-05-25 11:36:33 +0300

    first commit

---
+++ AOProject/AOProject/DbContexts/AODbContext.cs
@@ -0,0 +1,22 @@
+﻿using AOProject.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AOProject.API.DbContexts
+{
+    public class AODbContext : DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+      
+
+        public AODbContext(DbContextOptions<AODbContext> options)
+          : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog=AOData");
+        }
+    }
+}
